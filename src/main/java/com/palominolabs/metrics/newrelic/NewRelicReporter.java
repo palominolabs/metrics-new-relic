6e6068eddfa9d314d02a7c2d7a93548187ec4e2a
Marshall Pierce <marshall@palominolabs.com>
2013-09-27 14:47:37 -0700

    use metrics 3

--- src/main/java/com/palominolabs/metrics/newrelic/NewRelicReporter.java
+++ src/main/java/com/palominolabs/metrics/newrelic/NewRelicReporter.java
@@ -1,108 +1,67 @@
 package com.palominolabs.metrics.newrelic;
 
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.Metered;
+import com.codahale.metrics.MetricFilter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Sampling;
+import com.codahale.metrics.ScheduledReporter;
+import com.codahale.metrics.Snapshot;
+import com.codahale.metrics.Timer;
 import com.newrelic.api.agent.NewRelic;
-import com.yammer.metrics.core.Counter;
-import com.yammer.metrics.core.Gauge;
-import com.yammer.metrics.core.Histogram;
-import com.yammer.metrics.core.Metered;
-import com.yammer.metrics.core.Metric;
-import com.yammer.metrics.core.MetricName;
-import com.yammer.metrics.core.MetricProcessor;
-import com.yammer.metrics.core.MetricsRegistry;
-import com.yammer.metrics.core.Sampling;
-import com.yammer.metrics.core.Timer;
-import com.yammer.metrics.reporting.AbstractPollingReporter;
-import com.yammer.metrics.reporting.AbstractReporter;
-import com.yammer.metrics.stats.Snapshot;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import java.util.Map;
-import java.util.Set;
+import java.util.SortedMap;
 import java.util.concurrent.TimeUnit;
 
-public final class NewRelicReporter extends AbstractPollingReporter implements MetricProcessor<String> {
+public final class NewRelicReporter extends ScheduledReporter {
 
     private static final Logger logger = LoggerFactory.getLogger(NewRelicReporter.class);
 
     /**
-     * Convenience method to start a reporter on a registry.
-     *
-     * @param registry metrics registry to report on
-     * @param period   the period between successive outputs
-     * @param unit     the time unit of {@code period}
+     * @see ScheduledReporter#ScheduledReporter(MetricRegistry, String, MetricFilter, TimeUnit, TimeUnit)
      */
-    public static void enable(MetricsRegistry registry, long period, TimeUnit unit) {
-        NewRelicReporter reporter = new NewRelicReporter(registry, "new-relic-reporter");
-        reporter.start(period, unit);
-    }
-
-    /**
-     * @param registry the {@link MetricsRegistry} containing the metrics this reporter will report
-     * @param name     the reporter's name
-     * @see AbstractReporter#AbstractReporter(MetricsRegistry)
-     */
-    protected NewRelicReporter(MetricsRegistry registry, String name) {
-        super(registry, name);
+    public NewRelicReporter(MetricRegistry registry, String name, MetricFilter filter, TimeUnit rateUnit,
+        TimeUnit durationUnit) {
+        super(registry, name, filter, rateUnit, durationUnit);
     }
 
     @Override
-    public void run() {
-        final Set<Map.Entry<MetricName, Metric>> metrics = getMetricsRegistry().allMetrics().entrySet();
-
-        for (Map.Entry<MetricName, Metric> entry : metrics) {
-            try {
-                final Metric metric = entry.getValue();
-                metric.processWith(this, entry.getKey(), null);
-            } catch (Exception e) {
-                logger.warn("Could not process metric " + entry.getKey(), e);
-            }
+    public void report(SortedMap<String, Gauge> gauges, SortedMap<String, Counter> counters,
+        SortedMap<String, Histogram> histograms, SortedMap<String, Meter> meters, SortedMap<String, Timer> timers) {
+        for (Map.Entry<String, Gauge> gaugeEntry : gauges.entrySet()) {
+            doGauge(gaugeEntry.getKey(), gaugeEntry.getValue());
         }
-    }
 
-    @Override
-    public void processMeter(MetricName name, Metered meter, String context) throws Exception {
-        doMetered(buildNameString(name), meter);
-    }
-
-    @Override
-    public void processCounter(MetricName name, Counter counter, String context) throws Exception {
-        NewRelic.recordMetric(buildNameString(name) + ".count", counter.count());
-    }
-
-    @Override
-    public void processHistogram(MetricName name, Histogram histogram, String context) throws Exception {
-        doSampling(buildNameString(name), histogram);
-    }
-
-    @Override
-    public void processTimer(MetricName name, Timer timer, String context) throws Exception {
-        doMetered(buildNameString(name), timer);
-        doSampling(buildNameString(name), timer);
-
-        NewRelic.recordMetric(buildNameString(name) + ".mean", (float) timer.mean());
-    }
-
-    @Override
-    public void processGauge(MetricName name, Gauge<?> gauge, String context) throws Exception {
-        NewRelic.recordMetric(buildNameString(name), (Float) gauge.value());
-    }
-
-    @Nonnull
-    private String buildNameString(@Nullable MetricName name) {
-        if (name == null) {
-            return "";
+        for (Map.Entry<String, Counter> counterEntry : counters.entrySet()) {
+            NewRelic.recordMetric(counterEntry.getKey() + ".count", counterEntry.getValue().getCount());
         }
-        final String qualifiedTypeName = name.getGroup() + "." + name.getType() + "." + name.getName();
-        return name.hasScope() ? qualifiedTypeName + '.' + name.getScope() : qualifiedTypeName;
+
+        for (Map.Entry<String, Histogram> histogramEntry : histograms.entrySet()) {
+            doSampling(histogramEntry.getKey(), histogramEntry.getValue());
+        }
+
+        for (Map.Entry<String, Meter> meterEntry : meters.entrySet()) {
+            doMetered(meterEntry.getKey(), meterEntry.getValue());
+        }
+
+        for (Map.Entry<String, Timer> timerEntry : timers.entrySet()) {
+            doMetered(timerEntry.getKey(), timerEntry.getValue());
+            doSampling(timerEntry.getKey(), timerEntry.getValue());
+
+            NewRelic.recordMetric(timerEntry.getKey() + ".mean", (float) timerEntry.getValue().getMeanRate());
+        }
     }
 
     private void doMetered(String name, Metered meter) {
-        NewRelic.recordMetric(name + ".count", meter.count());
-        NewRelic.recordMetric(name + ".meanRate", (float) meter.meanRate());
-        NewRelic.recordMetric(name + ".1MinuteRate", (float) meter.oneMinuteRate());
+        NewRelic.recordMetric(name + ".count", meter.getCount());
+        NewRelic.recordMetric(name + ".meanRate", (float) meter.getMeanRate());
+        NewRelic.recordMetric(name + ".1MinuteRate", (float) meter.getOneMinuteRate());
     }
 
     private void doSampling(String name, Sampling sampling) {
@@ -110,5 +69,16 @@ public final class NewRelicReporter extends AbstractPollingReporter implements M
         NewRelic.recordMetric(name + ".median", (float) snapshot.getMedian());
         NewRelic.recordMetric(name + ".75th", (float) snapshot.get75thPercentile());
         NewRelic.recordMetric(name + ".99th", (float) snapshot.get99thPercentile());
+    }
+
+    private void doGauge(String name, Gauge gauge) {
+        Object gaugeValue = gauge.getValue();
+
+        if (gaugeValue instanceof Number) {
+            float n = ((Number) gaugeValue).floatValue();
+            if (!Float.isNaN(n) && !Float.isInfinite(n)) {
+                NewRelic.recordMetric(name, n);
+            }
+        }
     }
 }
